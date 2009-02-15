fcec080fd2ba923d9489a2340d550ecd7cf5a749
Kasper Peeters <kasper@pooh.(none)>
2009-02-15 11:28:40 +0000

    EOD

--- include/modglue/pipe.hh
+++ include/modglue/pipe.hh
@@ -4,21 +4,21 @@
    $Id: pipe.hh,v 1.16 2006/03/10 14:23:23 kp229 Exp $
 
 	Pipe class (generalises Unix standard pipes and the pipe() call)
-	Copyright (C) 2001  Kasper Peeters <k.peeters@damtp.cam.ac.uk>
+	Copyright (C) 2001-2009  Kasper Peeters <kasper.peeters@aei.mpg.de>
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 	
 */
 
